1e39980b762e20a92ed0a22079de64747cf60e88
Christine <christinesunnjm@gmail.com>
2021-06-24 12:45:48 -0400

    photos properly load

--- PhotosViewController.m
+++ PhotosViewController.m
@@ -6,6 +6,8 @@
 //
 
 #import "PhotosViewController.h"
+#import "PhotoCell.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface PhotosViewController () <UITableViewDelegate, UITableViewDataSource>
 
@@ -22,6 +24,7 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
+    self.tableView.rowHeight = 288;
     
     NSURL *url = [NSURL URLWithString:@"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
@@ -33,13 +36,13 @@
             else {
                 NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
 
-                // TODO: Get the posts and store in posts property
+                // Get the posts and store in posts property
                 // Get the dictionary from the response key
                 NSDictionary *responseDictionary = dataDictionary[@"response"];
                 // Store the returned array of dictionaries in our posts property
                 self.posts = responseDictionary[@"posts"];
                 
-                // TODO: Reload the table view
+                // Reload the table view
                 [self.tableView reloadData];
             }
         
@@ -53,9 +56,29 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [[UITableViewCell alloc]init];
     
-    cell.textLabel.text = [NSString stringWithFormat:@"row %ld", (long)indexPath.row];
+    PhotoCell *cell = [tableView dequeueReusableCellWithIdentifier: @"PhotoCell"];
+    
+    if (cell) {
+        NSDictionary *post = self.posts[indexPath.row];
+        NSArray *photos = post[@"photos"];
+        if (photos) {
+            // 1. Get the first photo in the photos array
+            NSDictionary *photo = photos[0];
+
+            // 2. Get the original size dictionary from the photo
+            NSDictionary *originalSize = photo[@"original_size"];
+
+            // 3. Get the url string from the original size dictionary
+            NSString *urlString = originalSize[@"url"];
+
+            // 4. Create a URL using the urlString
+            NSURL *url = [NSURL URLWithString:urlString];
+            
+            // download the image to be associated with this cell
+            [cell.photoView setImageWithURL:url];
+        }
+    } else NSLog(@"nil");
     
     return cell;
 }
